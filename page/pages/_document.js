1b4baa242d24093d4b463cbca74fd9bed94bdeea
Amr <amrthabi7@gmail.com>
2022-10-05 16:56:22 -0400

    add lang="en" to html

---
+++ page/pages/_document.js
@@ -0,0 +1,19 @@
+import { Html, Head, Main, NextScript } from "next/document";
+
+export default function Document() {
+  return (
+    <Html lang="en">
+      <Head>
+        <title>Forum</title>
+        <meta
+          name="viewport"
+          description="Forum"
+        />
+      </Head>
+      <body>
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
+  );
+}
